e59c0fda4c6e45a0d2de6368c43969627b8c87b5
kepaperez <kepaperezdev@gmmail.com>
2023-09-28 15:47:52 +0200

    jwt tokens

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns=[
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('',views.getItems),
     path('<int:item_id>/', views.getItem),
     path('add/',views.addItem),
