3b026958936f3fa2885b6059d91c0bb8e8048fbf
kepaperez <kepaperezdev@gmmail.com>
2023-11-28 09:16:16 +0100

    images6

--- myproyect/settings.py
+++ myproyect/settings.py
@@ -120,6 +120,10 @@ CORS_ALLOWED_ORIGINS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+
+]
 
 # Optional: Allow credentials (cookies) in cross-origin requests
 CORS_ALLOW_CREDENTIALS = True
@@ -210,7 +214,7 @@ STATIC_URL = '/static/'
 # Following settings only make sense on production and may break development environments.
 if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    TATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
