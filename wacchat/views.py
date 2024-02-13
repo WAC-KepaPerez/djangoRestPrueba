367b4a3d7ac8355c57213ab17ba772742f752ad7
kepaperez <kepaperezdev@gmmail.com>
2024-02-13 14:38:14 +0100

    subir post de worpess a pinecone

--- wacchat/views.py
+++ wacchat/views.py
@@ -23,38 +23,39 @@ def pon(request):
 
 
 class SubirPost(APIView):
-    def post(self, request):
-        pc = Pinecone(api_key=PINECODE_API_KEY)
-        index = pc.Index('movies') 
-        vectors=[]
-        body_unicode = request.body.decode('utf-8')
-        post = json.loads(body_unicode)
-      
-        if not request.body:
-          return JsonResponse({'error': 'Empty request body'}, status=400)
-        try:
-            postData=  str(post)
-            response = client.embeddings.create(
-            input=postData,
-            model="text-embedding-3-small"
-            )
-            print(response.data[0].embedding)
-            vectors.append(
-              {
-              "id": str(post['id']), 
-              "values": response.data[0].embedding, 
-              "metadata": post
-              },
-            )
-            response=index.upsert(
-                vectors=vectors,
-            namespace="ns1"
-            )
-            print(response)
-            return Response({'status': "okey"}, status=status.HTTP_200_OK)
-        except Exception as e:
-            print("An error occurred:", str(e))
-            return Response({'status': "error", "error":e}, status=status.HTTP_400_BAD_REQUEST)   
+  def post(self, request):
+    pc = Pinecone(api_key=PINECODE_API_KEY)
+    index = pc.Index('movies') 
+    vectors=[]
+    body_unicode = request.body.decode('utf-8')
+    post = json.loads(body_unicode)
+
+    if not request.body:
+      return JsonResponse({'error': 'Empty request body'}, status=400)
+    
+    try:
+      postData=  str(post)
+      response = client.embeddings.create(
+      input=postData,
+      model="text-embedding-3-small"
+      )
+      #print(response.data[0].embedding)
+      vectors.append(
+        {
+        "id": str(post['id']), 
+        "values": response.data[0].embedding, 
+        "metadata": post
+        },
+      )
+      response=index.upsert(
+          vectors=vectors,
+      namespace="ns1"
+      )
+      print(response)
+      return Response({'status': "succes"}, status=status.HTTP_200_OK)
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return Response({'status': "error", "error":e}, status=status.HTTP_400_BAD_REQUEST)   
 
 
 
